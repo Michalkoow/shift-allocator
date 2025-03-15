c63700d7b58a0103829cd538e70566b9bbc85c76
Michalkoow <michalkoow@gmail.com>
2025-03-15 00:45:14 +0000

    dodanie przycisków edit i delete

--- projekt_koncowy/employees/views.py
+++ projekt_koncowy/employees/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Employee  # Usunięcie błędnego importu assign_employees
 from .forms import EmployeeForm
 from .utils import assign_employees  # Poprawny import funkcji z utils.py
 
 def employee_list(request):
     employees = Employee.objects.all()
+    print(f"Pobrani pracownicy w widoku: {employees}")  # Debugowanie - sprawdzenie co Django widzi
     return render(request, 'employees/employee_list.html', {'employees': employees})
+
 
 def add_employee(request):
     if request.method == 'POST':
@@ -21,3 +23,25 @@ def add_employee(request):
 def assign_employees_view(request):
     assignments = assign_employees()  # Wywołanie funkcji do przypisania pracowników
     return render(request, 'employees/assign_employees.html', {'assignments': assignments})
+
+
+def edit_employee(request, employee_id):
+    employee = get_object_or_404(Employee, id=employee_id)  # Pobranie pracownika
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            return redirect('employee_list')  # Powrót do listy pracowników
+    else:
+        form = EmployeeForm(instance=employee)
+
+    return render(request, 'employees/edit_employee.html', {'form': form, 'employee': employee})
+
+# Usuwanie pracownika
+def delete_employee(request, employee_id):
+    employee = get_object_or_404(Employee, id=employee_id)
+    if request.method == 'POST':
+        employee.delete()
+        return redirect('employee_list')
+
+    return render(request, 'employees/delete_employee.html', {'employee': employee})
